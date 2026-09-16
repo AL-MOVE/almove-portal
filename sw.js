5ac7963a50128267b4c95bfb4379342e69d0c1aa
AL-MOVE <amsm.almove@gmail.com>
2026-09-16 12:48:15 +0100

    Increment cache version to v47
    Updated cache version to ensure users receive the latest index.html.

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // A versão sobe com esta atualização visual para que instalações existentes
 // recebam o novo index.html em vez de manterem a versão anterior em cache.
 
-const VERSAO_CACHE = 'almove-portal-v46';
+const VERSAO_CACHE = 'almove-portal-v47';
 
 const FICHEIROS_ESSENCIAIS = [
   '/',
